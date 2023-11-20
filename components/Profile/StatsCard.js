e91d8d14f2d7cecaedb278a1a3a63ad34fb92b81
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-20 16:44:07 +0530

    Add Profile component and StatsCard component

---
+++ components/Profile/StatsCard.js
@@ -0,0 +1,41 @@
+import Likes from "@/public/icons/Likes";
+import Loved from "@/public/icons/Loved";
+import Stars from "@/public/icons/Stars";
+import Views from "@/public/icons/Views";
+import { abbreviateNumber } from "js-abbreviation-number";
+import React from "react";
+
+export default function StatsCard({ statsFor, statsValue }) {
+	if (statsFor === "followers" || statsFor === "following") {
+		return (
+			<p className="text-gray text-center mt-2 capitalize">
+				<span className="block border border-2 border-gray rounded-md text-center text-base font-bold opacity-80 px-4">
+					{abbreviateNumber(statsValue)}
+				</span>
+				{statsFor}
+			</p>
+		);
+	} else {
+		return (
+			<>
+				<div className="text-slate-600 flex items-center">
+					{(() => {
+						switch (statsFor) {
+							case "stars":
+								return <Stars />;
+							case "likes":
+								return <Likes />;
+							case "views":
+								return <Views />;
+							case "loved":
+								return <Loved />;
+						}
+					})()}
+					<span className="text-sm ml-1">
+						{abbreviateNumber(statsValue)}
+					</span>
+				</div>
+			</>
+		);
+	}
+}
---
+++ public/data.js
@@ -0,0 +1,18 @@
+export const userData = {
+	coverPicture:
+		"https://i0.wp.com/www.benday.com/wp-content/uploads/2015/03/paris-bridge-horizontal.jpg?resize=768%2C384&ssl=1",
+	name: "Anuj Gosalia",
+	followers: 6482,
+	following: 245,
+	verified: true,
+	profilePicture:
+		"https://pbs.twimg.com/profile_images/1148507476578275328/fsrvymGR_400x400.jpg",
+	bio: "Co-founder & CEO at Terribly Tiny Tales",
+	instagram: "https://www.instagram.com/anujgosalia",
+	stats: {
+		stars: 125,
+		likes: 12,
+		views: 57800,
+		loved: 26000,
+	},
+};
