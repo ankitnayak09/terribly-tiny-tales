73cdce801a5dee987b710592a095a01bdf22e4d2
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-20 15:16:52 +0530

    Add navigation bar with logo, stories, and courses buttons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Navbar() {
-	return <div>Navbar</div>;
+	return (
+		<nav className="flex justify-between items-center px-5 py-2 bg-[#111111FF]">
+			<div className="flex gap-2 items-center logo uppercase text-[#E8E8E8FF] font-medium tracking-[.25rem]">
+				<img src="./logo.svg" alt="" />
+				<div className="border border-transparent border-l-[#FFC13EFF] pl-2">
+					Stories
+				</div>
+			</div>
+			<button className="bg-[#FFC13EFF] text-[#462000FF] font-bold p-2 rounded-md">
+				Courses
+			</button>
+		</nav>
+	);
 }
