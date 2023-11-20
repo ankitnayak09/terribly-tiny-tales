3a233587b944c229bb7f706f8cd72296c38785ae
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-11-20 15:59:09 +0530

    Update Navbar styling and add new colors

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,12 +4,12 @@ export default function Navbar() {
 	return (
 		<nav className="flex justify-between items-center px-5 py-2 bg-[#111111FF]">
 			<div className="flex gap-2 items-center logo uppercase text-[#E8E8E8FF] font-medium tracking-[.25rem]">
-				<img src="./logo.svg" alt="" />
-				<div className="border border-transparent border-l-[#FFC13EFF] pl-2">
+				<img src="./logo.svg" alt="" className="w-[24px]" />
+				<div className="border border-transparent border-l-orangeYellow pl-2 text-xs">
 					Stories
 				</div>
 			</div>
-			<button className="bg-[#FFC13EFF] text-[#462000FF] font-bold p-2 rounded-md">
+			<button className="bg-orangeYellow text-[#462000FF] font-bold p-1 px-2 rounded-md">
 				Courses
 			</button>
 		</nav>
